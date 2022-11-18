07423c9ad2ed02b6d59a19bfe7900185de5e284b
Lucas A <contato.lucasalv@gmail.com>
2022-11-18 16:27:46 -0300

    refactor: use form hook

--- virtual-card-front/src/hooks/useForm.js
+++ virtual-card-front/src/hooks/useForm.js
@@ -1,9 +1,9 @@
 import { useState } from "react"
 
 const useForm = (initState = {}) => {
-	const [form, setForm] = useState(initState || {})
+	const [form, setForm] = useState(initState)
 	const handleForm = ({ target: { value, name } }) => setForm({ ...form, [name]: value })
-	const resetForm = () => setForm(initState || {})
+	const resetForm = () => setForm(initState)
 
 	return [form, handleForm, resetForm]
 }
